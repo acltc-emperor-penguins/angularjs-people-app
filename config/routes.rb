ebc3473b2a1d0cc4ea6b49599b42e01144f9f250
Jay Wengrow <jaywngrw@gmail.com>
2015-07-20 21:14:42 -0500

    created api for people index.json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'people#index'
+  namespace :api do
+    namespace :v1 do
+      get "/people" => 'people#index'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
